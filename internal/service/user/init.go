a17023f0c2af3ec9767bf8c07c2cf9c28f40d83a
Malik Wali <malik.wali@salt.co.id>
2024-04-06 22:26:09 +0700

    feat: add goose migration

--- internal/service/user/init.go
+++ internal/service/user/init.go
@@ -1,1 +1,18 @@
 package user
+
+import (
+	"github.com/jmoiron/sqlx"
+)
+
+type userService struct {
+	db *sqlx.DB
+}
+
+type UserService interface {
+}
+
+func NewUserService(db *sqlx.DB) UserService {
+	return &userService{
+		db: db,
+	}
+}
