cbfc4dc6ffa843cc1bc39e75731e190f99e07942
Malik Wali <malik.wali@salt.co.id>
2024-05-16 23:19:31 +0700

    start over

--- config/init.go
+++
@@ -1,7 +0,0 @@
-package config
-
-import "github.com/joho/godotenv"
-
-func InitConfig() error {
-	return godotenv.Load()
-}
--- internal/delivery/http/v1/controller/users/handler/register_handler.go
+++
@@ -1,7 +0,0 @@
-package userHandler
-
-import "github.com/gofiber/fiber/v2"
-
-func (uh userHandler) Register(c *fiber.Ctx) error {
-	return nil
-}
