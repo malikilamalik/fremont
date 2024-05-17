d881a38155274901793267c5f137ff3c3e975e9d
Malik Wali <malik.wali@salt.co.id>
2024-05-17 23:20:36 +0700

    feat: server config

---
+++ config/server_config.go
@@ -0,0 +1,17 @@
+package config
+
+import "os"
+
+func ServerPort() string {
+	if os.Getenv("APP_PORT") == "" {
+		return ":8080"
+	}
+	return ":" + os.Getenv("APP_PORT")
+}
+
+func ServerEnvirontment() string {
+	if os.Getenv("APP_ENV") == "" {
+		return "local"
+	}
+	return ":" + os.Getenv("APP_ENV")
+}
--- server/server.go
+++ server/server.go
@@ -1,1 +1,22 @@
 package server
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/malikilamalik/freemont/config"
+)
+
+type Server struct {
+	app *echo.Echo
+}
+
+func NewServer() *Server {
+	e := echo.New()
+
+	return &Server{
+		app: e,
+	}
+}
+
+func (s *Server) Run() error {
+	return s.app.Start(config.ServerPort())
+}
